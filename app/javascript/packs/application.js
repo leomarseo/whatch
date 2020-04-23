8b86812763b9d8dd9cfb631863e78ae2800df2d7
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-04-23 17:58:33 +0200

    basic autocomplete works for actors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,12 @@ require("channels")
 // require("packs/searchfilters")
 
 import { searchFilters } from './searchfilters.js'
+import { autocompleteSearch } from './autocomplete.js'
 import "bootstrap";
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // myCustomJS();
 });
 
   searchFilters();
+  autocompleteSearch();
+
--- app/javascript/packs/searchfilters.js
+++ app/javascript/packs/searchfilters.js
@@ -1,37 +1,36 @@
-
 const searchFilters = () => {
   console.log("the searchfilters.js file was loaded successfully")
 // here we get the user selection
   let searchContainer = document.getElementById("mySelect");
 
 // this function reveals the searchbar with the corresponding user selection
-function showSearchbar() {
-  let x = document.getElementById("mySelect").value;
-  let y = document.getElementById("search-bar").classList.item(0);
-  document.getElementById("search-bar").style.display = "block";
-  document.getElementById("search-bar").classList.remove(y);
-  document.getElementById("search-bar").classList.add(x);
-  document.getElementById("search-bar-field").placeholder = x;
-}
+  function showSearchbar() {
+    let x = document.getElementById("mySelect").value;
+    let y = document.getElementById("search-bar").classList.item(0);
+    document.getElementById("search-bar").style.display = "block";
+    document.getElementById("search-bar").classList.remove(y);
+    document.getElementById("search-bar").classList.add(x);
+    document.getElementById("search-bar-field").placeholder = x;
+  }
 
-// this is part of the autocomplete
-searchContainer.addEventListener("change", showSearchbar);
+  // this is part of the autocomplete
+  searchContainer.addEventListener("change", showSearchbar);
 
-// this is a listener for the add button
-const addButton = document.getElementById("add-button");
-addButton.addEventListener("click", (event) => {
-  let currentSelection = document.getElementById("search-bar").classList.item(0);
-  let currentTextInput = document.getElementById("search-bar-field").value;
-  document.querySelector(`.${currentSelection}s_positive`).value= document.querySelector(`.${currentSelection}s_positive`).value + "," + currentTextInput;
-})
+  // this is a listener for the add button
+  const addButton = document.getElementById("add-button");
+  addButton.addEventListener("click", (event) => {
+    let currentSelection = document.getElementById("search-bar").classList.item(0);
+    let currentTextInput = document.getElementById("search-bar-field").value;
+    document.querySelector(`.${currentSelection}s_positive`).value= document.querySelector(`.${currentSelection}s_positive`).value + "," + currentTextInput;
+  })
 
-const removeButton = document.getElementById("remove-button");
-// now for the remove button
-removeButton.addEventListener("click", (event) => {
-  let currentSelection = document.getElementById("search-bar").classList.item(0);
-  let currentTextInput = document.getElementById("search-bar-field").value;
-  document.querySelector(`.${currentSelection}s_negative`).value = document.querySelector(`.${currentSelection}s_negative`).value + "," + currentTextInput;
-})
+  const removeButton = document.getElementById("remove-button");
+  // now for the remove button
+  removeButton.addEventListener("click", (event) => {
+    let currentSelection = document.getElementById("search-bar").classList.item(0);
+    let currentTextInput = document.getElementById("search-bar-field").value;
+    document.querySelector(`.${currentSelection}s_negative`).value = document.querySelector(`.${currentSelection}s_negative`).value + "," + currentTextInput;
+  })
 
 
 }
