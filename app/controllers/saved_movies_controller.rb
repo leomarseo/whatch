5c4ce4becf20489a9a1c3fa2817cc906fc0288bf
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-04-24 13:10:00 +0200

    saved_controller fixes

--- app/controllers/saved_movies_controller.rb
+++ app/controllers/saved_movies_controller.rb
@@ -10,22 +10,21 @@ class SavedMoviesController < ApplicationController
     @saved_movie.destroy
     redirect_to saved_movie_path(next_id)
   end
-end
 
-# @books = Book.includes(:author)
-# @saved_movies = SavedMovie.
-
-def find_next_id(current_movie)
-  @saved_movies = current_user.saved_movies.where(seen: false)
-  next_index = @saved_movies.index(current_movie) +1  # i take the index of the next movie in the array
-  if current_movie != @saved_movies.last # if the movie that i'm seeing is not the last one
-    next_id = @saved_movies[next_index].id # next_id is equal to the id of the next movie in the array (retrived by the next_index)
-  else
-    next_id = @saved_movies.first.id # otherwise the next_id is the id of the first object/movie in the @saved_movies array
+  def find_next_id(current_movie)
+    @saved_movies = current_user.saved_movies.where(seen: false)
+    next_index = @saved_movies.index(current_movie) +1  # i take the index of the next movie in the array
+    if current_movie != @saved_movies.last # if the movie that i'm seeing is not the last one
+      next_id = @saved_movies[next_index].id # next_id is equal to the id of the next movie in the array (retrived by the next_index)
+    else
+      next_id = @saved_movies.first.id # otherwise the next_id is the id of the first object/movie in the @saved_movies array
+    end
+    return next_id
   end
-  return next_id
-end
 
+  helper_method :find_next_id
+  # moved find_next_id inside the SavedMoviesController and used helper_method
+end
 
 
 
