3473b349f56a9220f188a154542c790eaa3ddb2d
Kevin Kim <khk271@nyu.edu>
2020-04-23 22:32:47 -0400

    created controller methods and routes for each card action - pass, later, seen

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,5 +1,7 @@
 class SuggestionsController < ApplicationController
 
+  before_action :set_suggestion_skip, only: [ :pass_suggestion, :move_to_later, :already_seen ]
+
   def index
     @suggestion = Suggestion.all
   end
@@ -17,6 +19,7 @@ class SuggestionsController < ApplicationController
   def show
     # the show accepts a parameter from the filters page
     # the @tmdb_suggestion declaration is temporary, until we successfully receive sample tmdb_suggestions
+
     @current_suggestion = []
     @tmdb_suggestion = TmdbSuggestion.first.suggestions
 
@@ -41,11 +44,11 @@ class SuggestionsController < ApplicationController
     redirect_to pages_suggestions_path
   end
 
-  def set_true
-    @suggestion = Suggestion.find(params[:suggestion_id])
-    @suggestion.update(skip: true)
-    redirect_to results_path
-  end
+  def pass_suggestion; end
+
+  def move_to_later; end
+
+  def already_seen; end
 
   # def destroy
   #   @suggestion = Suggestion.find(params[:id])
@@ -57,6 +60,12 @@ class SuggestionsController < ApplicationController
 
   def suggestion_params
     params.require(:suggestion).permit( :tmdb_suggestion_id, :movie_id, :skip)
+  end
+
+  def set_suggestion_skip
+    @suggestion = Suggestion.find(params[:suggestion_id])
+    @suggestion.update(skip: true)
+    redirect_to results_path
   end
 
 end
