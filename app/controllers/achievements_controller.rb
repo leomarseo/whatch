40b496e8e0715a04b2535e649f9d9795271c97f2
Gian Marco Romani <gromani92@gmail.com>
2020-04-25 17:41:55 +0200

    Added method to retrieve achievements

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,10 +1,13 @@
+# At ht e bottom of the page i set some variables in the set_variables method :*
+
 class AchievementsController < ApplicationController
-  before_action :n_movie_achievement, only: [:index]
   before_action :set_variables
 
   def index
     @achievements = Achievement.all
   end
+
+  #VISUALISATIONS
 
   def achievement_loop_visualisation
     @achievements = Achievement.where(category: "visualisations")
@@ -15,71 +18,175 @@ class AchievementsController < ApplicationController
     end
   end
 
-  def achievement_loop_adult #controllare che adult sia una categoria e non un boolean del movie
-    @movies = @seen_movies.movie.genres.where(name: "adult")
-    @achievements = Achievement.where(category: "adult")
+  #APP
+
+  def achievement_loop_number_queries
+    @achievements = Achievement.where(category: "app")
     @achievements.each do |achievement|
-      if @movies.count >= achievement.number && @joint_achievements.find_by(achievement_id: achievement.id).nil?
+      if user.tmdb_suggestions.count > achievements.number && @joint_achievements.find_by(achievement_id: achievement.id).nil?
         JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
       end
     end
   end
 
-  def achievement_loop_documentary
-    @movies = @seen_movies.movie.genres.where(name: "documentary")
-    @achievements = Achievement.where(category: "documentary")
-    @achievements.each do |achievement|
-      if @movies.count >= achievement.number && @joint_achievements.find_by(achivement_id: achievement.id).nil?
-        JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
-      end
-    end
-  end
+  #HOURS
 
   def achievement_loop_hours
-    @movies_total_duration = @seen_movies.movie.genres.where(name: "documentary")
-    @achievements = Achievement.where(category: "documentary")
+    @movies_total_runtime = 0
+    @seen_movies.each do |seen_movie|
+      @movies_total_runtime += seen_movie.runtime
+    end
+    @achievements = Achievement.where(category: "hours")
     @achievements.each do |achievement|
-      if @movies.count >= achievement.number && @joint_achievements.find_by(achivement_id: achievement.id).nil?
+      if @movies_total_runtime >= achievement.number && @joint_achievements.find_by(achivement_id: achievement.id).nil?
         JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
       end
     end
   end
 
+  #CATEGORY-DOCUMENTARY
 
+  def achievement_loop_documentary
+    @movies_count = 0
+    @seen_movies.each do |seen_movie|
+      seen_movie.movie.joint_genres.each do |joint_genre|
+        if joint_genre.genre.name == "documentary"
+          @movies_count += 1
+        end
+      end
+    end
+    @achievements = Achievement.where(category: "documentary")
+    @achievements.each do |achievement|
+      if @movies_count >= achievement.number && @joint_achievements.find_by(achivement_id: achievement.id).nil?
+        JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
+      end
+    end
+  end
 
+  #CATEGORY-WESTERN
+
+  def achievement_loop_western
+    @movies_count = 0
+    @seen_movies.each do |seen_movie|
+      seen_movie.movie.joint_genres.each do |joint_genre|
+        if joint_genre.genre.name == "western"
+          @movies_count += 1
+        end
+      end
+    end
+    @achievements = Achievement.where(category: "western")
+    @achievements.each do |achievement|
+      if @movies_count >= achievement.number && @joint_achievements.find_by(achivement_id: achievement.id).nil?
+        JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
+      end
+    end
+  end
+
+  #DIRECTORS
+
+  def achivement_loop_directors
+    @seen_movies_directed_by_this_director = 0
+    @all_movies_directed_by_this_director = Director.find_by(name: "Werner Herzog").movies.count
+
+    @seen_movies.each do |seen_movie|
+      seen_movie.movie.directors.each do |director|
+        if director.name == "Werner Herzog"
+          @seen_movies_directed_by_this_director += 1
+        end
+      end
+    end
+    @achievements = Achievement.where(category: "actors")
+    @achievements.each do |achievement|
+      if @seen_movies_directed_by_this_director == @all_movies_directed_by_this_director && @joint_achievements.find_by(achievement_id: achievement.id).nil?
+        JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
+      end
+    end
+  end
+
+  #ACTORS
+
+  def achivement_loop_actors
+    @seen_movies_with_this_star = 0
+    @all_movies_with_this_star = Actor.find_by(name: "Brad Pitt").movies.count
+
+    @seen_movies.each do |seen_movie|
+      seen_movie.movie.starring_actors.each do |starring_actor|
+        if starring_actor.actor.name == "Brad Pitt"
+          @seen_movies_with_this_star += 1
+        end
+      end
+    end
+    @achievements = Achievement.where(category: "actors")
+    @achievements.each do |achievement|
+      if @seen_movies_with_this_star == @all_movies_with_this_star && @joint_achievements.find_by(achievement_id: achievement.id).nil?
+        JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
+      end
+    end
+  end
+
+  #WEIRD
+
+  #Many queries but nothing watched
+
+  def achievement_loop_nothing_watched
+    @achievements = Achievement.where(category: "nothing_watched")
+    @achievement.each do |achievement|
+      if user.tmdb_suggestions.count > 15 && @seen_movies.count == 0 && @joint_achievements.find_by(achivement_id: achievement.id).nil?
+        JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
+      end
+    end
+  end
+
+  #Watched many movies with low rating
+  def achievement_loop_low_rating
+    @low_rating_movies = []
+    @seen_movies.each do |seen_movie|
+      if seen_movie.movie.vote_average < 5.9
+        @low_rating_movies << seen_movie
+      end
+    end
+    @achievements = Achievements.where(category: "low-rating")
+    @achievements.each do |achievement|
+      if @low_rating_movies.count >= 10 && @joint_achievements.find_by(achivement_id: achievement.id).nil?
+        JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
+      end
+    end
+  end
+
+  #Skip
+
+  def achievement_loop_skip
+    @skipped_movies = current_user.suggestions.where(skip: true).count
+    @achievements= Achievement.where(category: "skip")
+    @achievements.each do |achievement|
+      if @skipped_movies >= achievement.number && @joint_achievements.find_by(achivement_id: achievement.id).nil?
+        JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
+      end
+    end
+  end
+
+  #ADULT
+
+  def achievement_loop_adult
+    @adult_movies = 0
+    @seen_movies.each do |seen_movie|
+      if seen_movie.movie.adult == true
+        @adult_movies += 1
+      end
+    end
+    @achievements = Achievement.where(category: "adult")
+    @achievements.each do |achievement|
+      if @adult_movies >= achievement.number && @joint_achievements.find_by(achievement_id: achievement.id).nil?
+        JointAchievement.create(user_id: current_user.id, achievement_id: achievement.id, earned: true)
+      end
+    end
+  end
 
 
   def set_variables
     @seen_movies = current_user.saved_movies.where(seen: true)
     @joint_achievements = current_user.joint_achievements
+    @movies = Movies.all
   end
 
-  # genre = current_user.saved_movies.where(seen: true).last.movie.genres.where()
-
-  # def genre_achivement(genre)
-
-  # end
-
-  # helper_method :n_movie_achievement
-
-
-
-
-
-
-
-
 end # la classe si chiude qui
-
-  # def n_movie_achievement # a method to give achievements based on the number of movie you watched
-  #   if current_user.saved_movies.where(seen: true).count > 1 && current_user.joint_achievements.find_by(achievement_id: 1).nil?
-  #     JointAchievement.create(user_id: current_user.id, achievement_id: 1, earned: true)
-  #   # elsif @seen_movies.count == 5 && current_user.joint_achievement.earned = false
-  #   #   JointAchievement.create(user_id: 2, achievement_id: 2, earned: true)
-  #   # elsif @seen_movies.count == 10 && current_user.joint_achievement.earned = false
-  #   #   JointAchievement.create(user_id: 2, achievement_id: 3, earned: true)
-  #   # elsif @seen_movies.count == 100 && current_user.joint_achievement.earned = false
-  #   #   JointAchievement.create(user_id: 2, achievement_id: 4, earned: true)
-  #   # elsif @seen_movies.count == 500 && current_user.joint_achievement.earned = false
-  #   #   JointAchievement.create(user_id: 2, achievement_id: 5, earned: true)
-  #   end
--- app/jobs/create_achievements_job.rb
+++ app/jobs/create_achievements_job.rb
@@ -2,10 +2,11 @@ class CreateAchievementsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # VISUALISATIONS
-    Achievement.create(name: "Rookie", description: "You saw 1 movie", category: "visualisations", number: 1)
-    Achievement.create(name: "Beginner", description: "You saw 5 movies", category: "visualisations", number: 5)
-    Achievement.create(name: "Newby", description: "You saw 10 movies", category: "visualisations", number: 10)
+
+    # VISUALISATIONS 👍
+    Achievement.create(name: "Newbie", description: "You saw 1 movie", category: "visualisations", number: 1)
+    Achievement.create(name: "Rookie", description: "You saw 5 movies", category: "visualisations", number: 5)
+    Achievement.create(name: "Beginner", description: "You saw 10 movies", category: "visualisations", number: 10)
     Achievement.create(name: "Intermediate", description: "You saw 100 movies", category: "visualisations", number: 100)
     Achievement.create(name: "Expert", description: "You saw 500 movies", category: "visualisations", number: 500)
     Achievement.create(name: "Unstoppable", description: "You saw 1000 movies", category: "visualisations", number: 1000)
@@ -16,34 +17,42 @@ class CreateAchievementsJob < ApplicationJob
     Achievement.create(name: "You're back", description: "You saw all the Terminator", category: "collection", number: 6)
     Achievement.create(name: "Adrianaaaa!", description: "You saw all the Rocky collection", category: "collection", number: 6)
 
-    #CATEGORY - DOCUMENTARY
-    Achievement.create(name: "The professor", description: "You saw 10 documentary", category: "documentary", number: 6)
+    #CATEGORY - DOCUMENTARY 👍
+    Achievement.create(name: "The professor", description: "You saw 10 documentary", category: "documentary", number: 10)
 
-    #CATEGORY - ADULT
+    #CATEGORY - WESTERN 👍
+    Achievement.create(name: "Yeee-ha", description: "You saw 10 western movie", category: "western", number: 10)
+
+    #CATEGORY - COMEDY
+    Achievement.create(name: "Ha ha ha", description: "You saw 5 comedies", category: "comedy", number: 5)
+    Achievement.create(name: "Yeee-ha", description: "You saw 10 comedies", category: "comedy", number: 10)
+    Achievement.create(name: "Yeee-ha", description: "You saw 10 comedies", category: "comedy", number: 30)
+
+    #CATEGORY - ADULT 👍
     Achievement.create(name: "Amateur", description: "You saw 3 adult movies", category: "adult", number: 3)
-    Achievement.create(name: "It's cheaper than dating 🧻", description: "You saw 10 adult movies", category: "adult", number: 10)
+    Achievement.create(name: "It's cheaper than dating", description: "You saw 10 adult movies", category: "adult", number: 10)
 
-    #STRANGE
-    Achievement.create(name: "Fetishist", description: "You saw 10 movies with low rating", category: "weird", number: 10)
-    Achievement.create(name: "What's the matter with you", description: "You didn't watch anything", category: "weird", number: 15) #The user made the query but he actually didn't watch anything
-    Achievement.create(name: "Calm down", description: "You sskipped 100 movies", category: "weird", number: 100)
-    Achievement.create(name: "It's not Tinder", description: "You sskipped 500 movies", category: "weird", number: 500)
+    #STRANGE 👍
+    Achievement.create(name: "Fetishist", description: "You saw 10 movies with low rating", category: "low-rating", number: 10)
+    Achievement.create(name: "What's the matter with you", description: "You didn't watch anything", category: "nothing-watched", number: 15) #The user made the query but he actually didn't watch anything
+    Achievement.create(name: "Calm down", description: "You skipped 100 movies", category: "skip", number: 100)
+    Achievement.create(name: "It's not Tinder", description: "You skipped 500 movies", category: "skip", number: 500)
 
-    #APP
+    #APP 👍
     Achievement.create(name: "He who seeks finds", description: "You did 10 queries", category: "app", number: 10)
     Achievement.create(name: "Archeologist", description: "You did 50 queries", category: "app", number: 50)
     Achievement.create(name: "Query master", description: "You did 100 queries", category: "app", number: 100)
     Achievement.create(name: "Sherlock", description: "You did 1000 queries", category: "app", number: 1000)
 
-    #HOURS
-    Achievement.create(name: "", description: "You watched 20 hours of movies", category: "hours", number: 20)
-    Achievement.create(name: "Red eyes", description: "You watched 20 hours of movies", category: "hours", number: 100)
-    Achievement.create(name: "Whatcher pro", description: "You watched 20 hours of movies", category: "hours", number: 500)
+    #HOURS 👍
+    Achievement.create(name: "Watcher", description: "You watched 20 hours of movies", category: "hours", number: 1200)
+    Achievement.create(name: "Red eyes", description: "You watched 20 hours of movies", category: "hours", number: 6000)
+    Achievement.create(name: "Whatcher pro", description: "You watched 20 hours of movies", category: "hours", number: 30000)
 
-    #ACTORS
-    Achievement.create(name: "Brad forever", description: "You watched all the movies with Brad Pitt", category: "brad", number: )
+    #ACTORS 👍
+    Achievement.create(name: "Brad forever", description: "You watched all the movies starring Brad Pitt", category: "actors", number: 9)
 
-    #DIRECTORS
-    Achievement.create(name: "Nosferatu", description: "You watched all the movies directed by Werner Herzog", category: "herzog", number: )
+    #DIRECTORS 👍
+    Achievement.create(name: "Nosferatu", description: "You watched all the movies directed by Wrner Herzog", category: "directors", number: 9)
   end
 end
