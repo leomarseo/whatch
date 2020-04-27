b022aea175841403f3bda796986c5677d99bd1a8
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-04-27 18:19:59 +0200

    adding many jobs temporarily

---
+++ app/jobs/update_relevant_actors_job_5.rb
@@ -0,0 +1,28 @@
+class UpdateRelevantActorsJob5 < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    counter = 0
+    Movie.includes(:starring_actors).drop(40000).first(10000).each do |movie|
+
+      movie.starring_actors.drop(15).each do |starring_actor|
+        counter += 1
+        StarringActor.find(starring_actor.id).destroy
+      end
+    end
+
+
+    puts "destroyed #{counter} starring_actors"
+
+    # actor_counter = 0
+    # Actor.includes(:starring_actors).each do |actor|
+
+    #   if actor.starring_actors == []
+    #     Actor.find(actor.id).destroy
+    #     actor_counter += 1
+    #   end
+    # end
+
+    # puts "destroyed #{actor_counter} actors"
+  end
+end
