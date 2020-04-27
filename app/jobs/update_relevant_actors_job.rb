61239fe6e241bf447548bb24f0f8aaf3fc856f40
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-04-27 18:04:39 +0200

    fixing job

--- app/jobs/update_relevant_actors_job.rb
+++ app/jobs/update_relevant_actors_job.rb
@@ -2,7 +2,7 @@ class UpdateRelevantActorsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Movie.includes(:starring_actors).each do |movie|
+    Movie.includes(:starring_actors).first(10000).each do |movie|
 
       movie.starring_actors.drop(15).each do |starring_actor|
         StarringActor.find(starring_actor.id).destroy
@@ -10,15 +10,15 @@ class UpdateRelevantActorsJob < ApplicationJob
     end
 
 
-    actor_counter = 0
-    Actor.includes(:starring_actors).each do |actor|
+    # actor_counter = 0
+    # Actor.includes(:starring_actors).each do |actor|
 
-      if actor.starring_actors == []
-        Actor.find(actor.id).destroy
-        actor_counter += 1
-      end
-    end
+    #   if actor.starring_actors == []
+    #     Actor.find(actor.id).destroy
+    #     actor_counter += 1
+    #   end
+    # end
 
-    puts "destroyed #{actor_counter} actors"
+    # puts "destroyed #{actor_counter} actors"
   end
 end
