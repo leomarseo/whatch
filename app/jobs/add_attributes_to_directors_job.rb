db416d7b784fb1614bf1f0bf59a0eea046b4cd12
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-04-29 15:53:17 +0200

    fix job

--- app/jobs/add_attributes_to_directors_job.rb
+++ app/jobs/add_attributes_to_directors_job.rb
@@ -5,12 +5,13 @@ class AddAttributesToDirectorsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Director.order(id: :asc).each do |director|
+    Director.order(id: :asc).offset(1).each do |director|
+      # the 1 offset is to prevent our placeholder director to be involved in this
       url = "https://api.themoviedb.org/3/person/#{director.tmdb_id}?api_key=81c398dbb6b994e4f815e69325c4893c&language=en-US"
       begin
         url_details = open(url).read
       rescue OpenURI::HTTPError
-        next # skips the whole creation process if url is not found
+        next # skips the whole update process if url is not found
       else
         person_details = JSON.parse(url_details)
         director.update(
