d8ef05f1d51affe644bb48f5d3a53f208010587b
Leonardo Marsigli <leonardomarsigli@gmail.com>
2020-05-05 13:20:42 +0200

    cleaned jobs

--- app/jobs/scrape_service_information_job.rb
+++ app/jobs/scrape_service_information_job.rb
@@ -4,34 +4,23 @@ require "nokogiri"
 class ScrapeServiceInformationJob < ApplicationJob
   queue_as :default
 
-  # def perform(amount, starting)
-  #   Movie.order(id: :asc).limit(amount).offset(starting).each do |movie|
-  #     url = "https://www.themoviedb.org/movie/#{movie.tmdb_id}/watch?language=it-IT"
-  #     html_file = open(url).read
-  #     html_doc = Nokogiri::HTML(html_file)
-
-  #     results = html_doc.search('.providers')
-  #     p results
-  #   end
-
-  # end
-
   def perform
-    url = "https://www.themoviedb.org/movie/284053/watch?language=it-IT"
-    html_doc = Nokogiri::HTML(open(url))
+    # LUIGI'S SCRAPER
 
-    link_array = nil
-    title_array = nil
-
-    html_doc.css('ul.providers li.ott_filter_4k').each do |link|
-      link.at_css('a').each do |node|
-        p node
-      end
+    tmbdid = 106646
+    url = "https://www.themoviedb.org/movie/#{tmbdid}/watch"
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    movie = {
+      tmbdid: tmbdid,
+      netflix: "",
+      timvision: ""
+    }
+    html_doc.search('.providers a').each do |element|
+      movie[:netflix] = element.attribute('href').to_s if element.attribute('title').to_s.include? 'Netflix'
+      movie[:timvision] = element.attribute('href').to_s if element.attribute('title').to_s.include? 'Tim'
     end
-
-    # puts link_array
-    # puts 'stop'
-    # puts title_array
+    puts movie
 
   end
 end
